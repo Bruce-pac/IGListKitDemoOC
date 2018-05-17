55ed5e6a6520a65176ee34f2e0647acd71faec58
gxy <guxinye@dinghaotech.com>
2018-05-17 21:59:58 +0800

    Initial Commit

---
+++ IGListKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IGListKitDemo
+//
+//  Created by gxy on 2018/5/17.
+//  Copyright © 2018年 gxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IGListKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IGListKitDemo
+//
+//  Created by gxy on 2018/5/17.
+//  Copyright © 2018年 gxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
